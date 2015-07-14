e23510ae4bd0ba55043b6a6e9651aba134cf145b
sethdame <seth_dame@yahoo.com>
2015-07-13 19:51:27 -0600

    first commit

--- practice.js
+++ practice.js
@@ -1,10 +1,12 @@
 //Once you complete a problem, open up Chrome and check the answer in the console.
 
 
-//Create an object called me. Give it a key of name with the value being your name, and another key of age with the value being your age. Then alert your name using dot notation.
+//Create an object called me. Give it a key of name with the value being your name, and another key of age with the value being your age. 
+//Then alert your name using dot notation.
 
   //Code here
-
+var me = {name:"Seth", age: 27};
+  me.name.alert;
 
 
 
@@ -13,20 +15,25 @@
 
 
 
-//Make a 'favoriteThings' object that contains the following keys: band, food, person, book, movie, holiday. Have the values to those keys be your favorite thing in that category.
+//Make a 'favoriteThings' object that contains the following keys: band, food, person, book, movie, holiday. 
+//Have the values to those keys be your favorite thing in that category.
 
-  //Code here
+var favoriteThings = {band:"Foo Fighters", food:"pizza", person:"RDJ", book:"The things they carried", movie:"Fight Club", holiday:"Cinco De Mayo"}
+  //Code here}
 
 
-//After you've made your object, add another key named 'car' with the value being your favorite car and then another key named 'brand' with the value being your favorite brand.
-
+//After you've made your object, add another key named 'car' with the value being your favorite car and then another key 
+//named 'brand' with the value being your favorite brand.
+favoriteThings.car = "GTR";
+favoriteThings.brand = "apple";
   //Code here
 
 
 //Now change the food key in your favoriteThings object to be 'Lettuce' and change the book key in your favoriteThings object to be '50 Shades of Gray'.
 
   //Code here
-
+favoriteThings.food = "Lettuce";
+favoriteThings.book = "50 Shades of Gray";
 
 
 
@@ -43,8 +50,13 @@ Using dot notation, add another key (or property) to your backPack object
 that is named color, with the value being the color of your backpack. */
 
   //Code here
+var backPack = {};
+var item = "firstPocket";
+backPack[item] = "chapstick";
+backPack.color = "Black";
 
 //After you do the above, alert your entire backPack object.
+backPack.alert;
 
   //Code here
 
@@ -53,7 +65,7 @@ Alerting to see the data in your Object doesn't work so well.
 Instead, console.log your whole backPack object and then check out the console. */
 
   //Code here
-
+console.log(backPack)
 
 
 
@@ -62,9 +74,16 @@ Instead, console.log your whole backPack object and then check out the console. 
 
 
 
-//Create another 'me' object with the following properties name, age, height, gender, married, eyeColor, hairColor. Fill those properties in with the appropriate values.
+//Create another 'me' object with the following properties name, age, height, gender, married, eyeColor, hairColor. 
+//Fill those properties in with the appropriate values.
 
   //Code Here
+
+var me = {name: "Seth", age: 27, height: 6, gender: "male", status: "single", eyeColor: "brown", hairColor: "brown"};
+
+  for(var value in me) {
+    alert(me[value])
+  }
 
 //Now, loop through your object and alert every value. *Tyler --> 24 --> 6'0 --> Male, etc etc
 
@@ -81,11 +100,14 @@ Instead, console.log your whole backPack object and then check out the console. 
 //Create an Object called 'album' with 5 keys named different song titles that you make up, with the values being the length of each song.
 
   //Code Here
+  var album = {bongo: 5, drum: 4, guitar: 3, sing: 3, bass: 4};
 
 //Now, loop through your album object alerting every song title individually.
 
   //Code Here
-
+  for(var keys in album) {
+    alert(album[keys])
+  }
 
 
 
@@ -97,12 +119,19 @@ Instead, console.log your whole backPack object and then check out the console. 
 //Create an object called states that has 5 US states as properties with the values being their population (doesn't have to be accurate).
 
   //Code Here
-
+var states = {Utah: 2, California: 13, Oregon: 3, Washington: 4, Nevada: 2};
 //Now, loop through your states object and if the states population is greater than 30K, alert that state.
 
   //Code Here
+  for (var property in states) {
+    if (states.hasOwnProperty(property)) {
+        // do stuff
+    }
+}
 
-
+for(var key in states) {
+  alert(states[key]);
+}
 
 
 //NEXT PROBLEM
@@ -122,6 +151,7 @@ var user = {
 that each value is truthy. If it's not truthy, remove it from the object. */
 
   //Code Here
+  delete 
 
 //Once you get your truthy Object, Change the remaining values in the object to be specific to you (name: 'your name', username: 'your username'), rather than my information.
 
@@ -150,12 +180,12 @@ var user = {
 // name -> 'Tyler S. McGinnis', email -> 'tyler.mcginnis@devmounta.in'. Make that change.
 
   //Code Here
-
+user.name = 'Tyler S. McGinnis'
+user.email = 'tyler.mcginnis@devmounta.in'
 //Now call the sayName method that's on the user object which will alert the users email
 
   //Code Here
-
-
+user.sayName[]
 
 
 //NEXT PROBLEM
@@ -166,13 +196,19 @@ var user = {
 //Create an empty object called methodCollection.
 
   //Code Here
+var methodCollection = {}
 
 /*Now add two methods (functions that are properties on objects) to your methodCollection
 object. One called 'alertHello' which alerts 'hello' and another method called logHello
  which logs 'hello' to the console. */
 
   //Code Here
-
+methodCollection.alertHello = function(){
+                      alert("hello");
+}
+methodCollection.logHello = function(){
+                      log("hello");
+}
 //Now call your alertHello and logHello methods.
 
   //Code Here
